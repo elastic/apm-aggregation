70e6004123b282f463a6ba5f6f0acceb5ccbd9da
Carson Ip <carson.ip@elastic.co>
2023-07-06 09:33:26 +0100

    Use BenchmarkAggregateBatchSerial and BenchmarkAggregateBatchParallel

--- aggregators/aggregator_test.go
+++ aggregators/aggregator_test.go
@@ -11,7 +11,6 @@ import (
 	"net/netip"
 	"sort"
 	"strings"
-	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -1188,7 +1187,7 @@ func BenchmarkAggregateCombinedMetrics(b *testing.B) {
 	}
 }
 
-func benchmarkAggregateBatchConcurrent(b *testing.B, concurrency int) {
+func BenchmarkAggregateBatchSerial(b *testing.B) {
 	b.ReportAllocs()
 	agg, err := New(AggregatorConfig{
 		DataDir: b.TempDir(),
@@ -1218,21 +1217,13 @@ func benchmarkAggregateBatchConcurrent(b *testing.B, concurrency int) {
 			},
 		},
 	}
-	wg := sync.WaitGroup{}
 	b.ResetTimer()
 
-	for j := 0; j < concurrency; j++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for i := 0; i < b.N; i++ {
-				if err := agg.AggregateBatch(context.Background(), "test", batch); err != nil {
-					b.Fatal(err)
-				}
-			}
-		}()
+	for i := 0; i < b.N; i++ {
+		if err := agg.AggregateBatch(context.Background(), "test", batch); err != nil {
+			b.Fatal(err)
+		}
 	}
-	wg.Wait()
 
 	if agg.batch != nil {
 		if err := agg.batch.Commit(pebble.Sync); err != nil {
@@ -1248,11 +1239,57 @@ func benchmarkAggregateBatchConcurrent(b *testing.B, concurrency int) {
 	}
 }
 
-func BenchmarkAggregateBatchConcurrent(b *testing.B) {
-	for _, concurrency := range []int{1, 10, 100, 1000} {
-		b.Run(fmt.Sprintf("%d", concurrency), func(b *testing.B) {
-			benchmarkAggregateBatchConcurrent(b, concurrency)
-		})
+func BenchmarkAggregateBatchParallel(b *testing.B) {
+	b.ReportAllocs()
+	agg, err := New(AggregatorConfig{
+		DataDir: b.TempDir(),
+		Limits: Limits{
+			MaxSpanGroups:                         1000,
+			MaxSpanGroupsPerService:               100,
+			MaxTransactionGroups:                  1000,
+			MaxTransactionGroupsPerService:        100,
+			MaxServiceTransactionGroups:           1000,
+			MaxServiceTransactionGroupsPerService: 100,
+			MaxServices:                           100,
+			MaxServiceInstanceGroupsPerService:    100,
+		},
+		Processor:            noOpProcessor(),
+		AggregationIntervals: []time.Duration{time.Second, time.Minute, time.Hour},
+	}, zap.NewNop())
+	if err != nil {
+		b.Fatal(err)
+	}
+	batch := &modelpb.Batch{
+		&modelpb.APMEvent{
+			Processor: modelpb.TransactionProcessor(),
+			Event:     &modelpb.Event{Duration: durationpb.New(time.Millisecond)},
+			Transaction: &modelpb.Transaction{
+				Name:                "T-1000",
+				RepresentativeCount: 1,
+			},
+		},
+	}
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			if err := agg.AggregateBatch(context.Background(), "test", batch); err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+
+	if agg.batch != nil {
+		if err := agg.batch.Commit(pebble.Sync); err != nil {
+			b.Fatal(err)
+		}
+		if err := agg.batch.Close(); err != nil {
+			b.Fatal(err)
+		}
+		agg.batch = nil
+	}
+	if err := agg.db.Close(); err != nil {
+		b.Fatal(err)
 	}
 }
 
