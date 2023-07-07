ac8f3595a9a3970039f7a1f109959882dcced00a
Vishal Raj <vishal.raj@elastic.co>
2023-07-07 16:52:59 +0800

    Add stop gap measure notice

--- aggregators/internal/hdrhistogram/hdrhistogram.go
+++ aggregators/internal/hdrhistogram/hdrhistogram.go
@@ -25,6 +25,7 @@
 // THE SOFTWARE.
 
 // Package hdrhistogram provides an optimized histogram for sparse samples.
+// This is a stop gap measure until we have [packed histogram implementation](https://www.javadoc.io/static/org.hdrhistogram/HdrHistogram/2.1.12/org/HdrHistogram/PackedHistogram.html).
 package hdrhistogram
 
 import (
