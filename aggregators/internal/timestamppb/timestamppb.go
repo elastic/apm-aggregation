1a75213b817afe7c2fdebeae44b9430e3c7bad5c
Carson Ip <carsonip@users.noreply.github.com>
2023-07-11 14:02:47 +0100

    Store timestamp as uint64 in protobuf (#15)
    Store timestamp as uint64 in protobuf so no pointer is needed for timestamp field

---
+++ aggregators/internal/timestamppb/timestamppb.go
@@ -0,0 +1,19 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+// Package timestamppb provides helper functions to encode and decode time.Time
+// for protobuf.
+package timestamppb
+
+import "time"
+
+// TimeToPBTimestamp encodes a time.Time to Unix epoch nanos in uint64 for protobuf.
+func TimeToPBTimestamp(t time.Time) uint64 {
+	return uint64(t.UnixNano())
+}
+
+// PBTimestampToTime decodes a uint64 of Unix epoch nanos to a time.Time for protobuf.
+func PBTimestampToTime(timestamp uint64) time.Time {
+	return time.Unix(0, int64(timestamp)).UTC()
+}
