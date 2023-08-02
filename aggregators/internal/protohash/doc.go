52fc1be99078540cae429918b3dc516697d2b8ba
Andrew Wilkins <axw@elastic.co>
2023-08-02 11:37:52 +0800

    Fix package doc

--- aggregators/internal/protohash/doc.go
+++ aggregators/internal/protohash/doc.go
@@ -2,8 +2,7 @@
 // or more contributor license agreements. Licensed under the Elastic License 2.0;
 // you may not use this file except in compliance with the Elastic License 2.0.
 
-// Package protohash holds functions for hashing aggregationpb.*Key types.
-
 //go:generate go run ./generate
 
+// Package protohash holds functions for hashing aggregationpb.*Key types.
 package protohash
