14a8fc40fe2bfc4b69411b1605bdd06db9940501
Vishal Raj <vishal.raj@elastic.co>
2023-08-04 12:06:37 +0800

    Remove bad changes

--- aggregators/merger.go
+++ aggregators/merger.go
@@ -5,7 +5,6 @@
 package aggregators
 
 import (
-	"fmt"
 	"io"
 
 	"github.com/axiomhq/hyperloglog"
@@ -86,9 +85,6 @@ func (m *combinedMetricsMerger) merge(from *aggregationpb.CombinedMetrics) {
 	//         of the _to_ combined metrics.
 	for i := range from.ServiceMetrics {
 		fromSvc := from.ServiceMetrics[i]
-		if fromSvc.Key == nil {
-			fmt.Printf("FROM AGGREGATOR %+v\n", fromSvc)
-		}
 		serviceKeyHash := protohash.HashServiceAggregationKey(xxhash.Digest{}, fromSvc.Key)
 		var sk serviceAggregationKey
 		sk.FromProto(fromSvc.Key)
